5976486ef4542ae591d44f7aa71e357d1fb8043e
root <root@localhost.localdomain>
2014-01-02 01:15:06 +0000

    updating base and http

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -1,3 +1,3 @@
 class stringed::profile::base {
-  
+  include ::ntp
 }
--- manifests/profile/httpd.pp
+++ manifests/profile/httpd.pp
@@ -1,21 +1,3 @@
-class stringed::profile::httpd (
-  $ssl     = false,
-  $modules = [
-               'auth_basic',
-               'authn_file',
-               'authz_host',
-               'authz_user',
-               'mime',
-               'negotiation',
-               'dir',
-               'alias',
-               'rewrite',
-               'proxy'
-             ],
-) {
-  apache_httpd { 'prefork':
-    ssl     => $ssl,
-    modules => $modules,
-    welcome => false,
-  }
+class stringed::profile::httpd::bare {
+  class { 'apache':  }
 }
