cae2f177d7eecb2887ed9e7f2259e52e5ec7b2c4
root <root@localhost.localdomain>
2014-01-03 09:10:51 +0000

    updating package list and repos

--- manifests/general/packages.pp
+++ manifests/general/packages.pp
@@ -1,7 +1,26 @@
 class stringed::general::packages {
   # Duplicate what's in base image (less some installed by other modules):
-  #  ntp postfix sudo ldap bc bind-utils curl finger gnupg man mlocate nano vi patch rsync screen sysstat telnet unzip git puppet
-  package { [ postfix, sudo, ldap, bc, bind-utils, curl, finger, gnupg, man, mlocate, nano, vi, patch, rsync, screen, sysstat, telnet, unzip, git, puppet ]:
+  package { [
+               bc,
+               bind-utils,
+               curl,
+               finger,
+               git,
+               gnupg2,
+               man,
+               mlocate,
+               nano,
+#              ntp,
+               patch,
+#              postfix,
+#              puppet,
+               rsync,
+               screen,
+               sudo,
+               sysstat,
+               telnet,
+               unzip,
+            ]:
     ensure => latest,
   }
 }
