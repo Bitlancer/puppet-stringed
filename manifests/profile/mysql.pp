287101ca951ad8f2ff48d768a8c7826aa4812405
JCotton1123 <jcotton1123@gmail.com>
2014-01-23 18:18:52 -0500

    Update mysql.pp

--- manifests/profile/mysql.pp
+++ manifests/profile/mysql.pp
@@ -1,3 +1,10 @@
 class stringed::profile::mysql {
-  include ::mysql::server
+  class { '::mysql::server':
+    override_options => { 
+      'mysqld' => { 
+        'innodb-buffer-pool-size' => $innodb_buffer_pool_size,
+        'max-connections' => $max_connections
+      }
+    }
+  }
 }
