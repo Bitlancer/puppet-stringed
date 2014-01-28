4669e1c9283510a17f2491834ecb18dbc1e01d5f
JCotton1123 <jcotton1123@gmail.com>
2014-01-28 14:36:47 -0500

    Update rabbitmq.pp

--- manifests/profile/rabbitmq.pp
+++ manifests/profile/rabbitmq.pp
@@ -1,10 +1,7 @@
 class stringed::profile::rabbitmq {
   class { '::erlang':
     epel_enable => true
-  }
+  } ->
   class { '::rabbitmq':
-    package_provider => 'yum',
-    package_name => 'rabbitmq-server'
   }
-  Class['erlang'] -> Class['rabbitmq']
 }
