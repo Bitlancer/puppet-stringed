4b80686e7cf5eed2dc995136dfbd8eae7e7286de
JCotton1123 <jcotton1123@gmail.com>
2014-02-17 13:01:02 -0500

    Update firewall.pp

--- manifests/general/firewall.pp
+++ manifests/general/firewall.pp
@@ -1,7 +1,16 @@
 class stringed::general::firewall {
 
+  # This exists to work around a bug with the firewall module.
+  # See bug report at
+  # https://github.com/puppetlabs/puppetlabs-firewall/issues/310.
+  # Once this issue is resolved this exec call should be removed
+  # and purge should be set to true.
+  exec { "flush firewall":
+    command => "/sbin/iptables -F",
+    onlyif => "/bin/grep system-config-firewall /etc/sysconfig/iptables"
+  } ->
   resources { firewall:
-    purge => true
+    purge => false
   }
   Firewall {
     before  => Class['stringed::wrapper::post_firewall'],
