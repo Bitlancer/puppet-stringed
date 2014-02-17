c8639a329ba51f915c96304be723d118d75378c6
JCotton1123 <jcotton1123@gmail.com>
2014-02-17 16:30:47 -0500

    Update apache_phpfpm.pp

--- manifests/profile/apache_phpfpm.pp
+++ manifests/profile/apache_phpfpm.pp
@@ -1,3 +1,13 @@
-class stringed::profile::apache_phpfpm {
+class stringed::profile::apache_phpfpm (
+  $firewall_rules = {
+    '100 allow http and https access' => {
+      port => [80, 443],
+      proto => tcp,
+      action => accept
+    }
+  }
+)
+{
   include stringed::wrapper::apache_phpfpm
+  create_resources(firewall, $firewall_rules)
 }
