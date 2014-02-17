c39999bcf4d5e71711135f1bc42dade9aa7e5d08
JCotton1123 <jcotton1123@gmail.com>
2014-02-17 16:32:14 -0500

    Update httpd.pp

--- manifests/profile/httpd.pp
+++ manifests/profile/httpd.pp
@@ -1,3 +1,13 @@
-class stringed::profile::httpd::bare {
+class stringed::profile::httpd::bare (
+  $firewall_rules = {
+    '100 allow http and https access' => {
+      port => [80, 443],
+      proto => tcp,
+      action => accept
+    }
+  }
+)
+{
   include ::apache
+  create_resources(firewall, $firewall_rules)
 }
