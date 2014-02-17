47e07ff6f3f141d26ee6228c3ba15321770b5bba
JCotton1123 <jcotton1123@gmail.com>
2014-02-17 16:35:04 -0500

    Update mysql.pp

--- manifests/profile/mysql.pp
+++ manifests/profile/mysql.pp
@@ -1,5 +1,9 @@
-class stringed::profile::mysql {
+class stringed::profile::mysql (
+  $firewall_rules = {}
+)
+{
   class { '::mysql::server':
     override_options => hiera_hash('stringed::profile::mysql::override_options', {})
   }
+  create_resources(firewall, $firewall_rules)
 }
