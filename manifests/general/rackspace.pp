e30580f7d138800a57ca07e8a5d8bcc7c21bab8d
JCotton1123 <jcotton1123@gmail.com>
2014-02-18 17:24:01 -0500

    Work around for augeas bug

--- manifests/general/rackspace.pp
+++ manifests/general/rackspace.pp
@@ -1,12 +1,18 @@
 class stringed::general::rackspace {
   augeas { "network peerdns setup eth0":
     context => "/files/etc/sysconfig/network-scripts/ifcfg-eth0",
+    incl => "/etc/sysconfig/network-scripts/ifcfg-eth0",
+    load_path => [ "/usr/share/augeas/lenses/dist" ],
+    lens => "Shellvars.lns",
     changes => [
       "set PEERDNS no",
     ],
   }
   augeas { "network peerdns setup eth1":
     context => "/files/etc/sysconfig/network-scripts/ifcfg-eth1",
+    incl => "/etc/sysconfig/network-scripts/ifcfg-eth1",
+    load_path => [ "/usr/share/augeas/lenses/dist" ],
+    lens => "Shellvars.lns",
     changes => [
       "set PEERDNS no",
     ],
