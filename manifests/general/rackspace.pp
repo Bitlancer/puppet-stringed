604125c365ae865251061d1a1762b91c0c62d6eb
JCotton1123 <jcotton1123@gmail.com>
2014-02-18 17:25:57 -0500

    Update rackspace.pp

--- manifests/general/rackspace.pp
+++ manifests/general/rackspace.pp
@@ -2,7 +2,6 @@ class stringed::general::rackspace {
   augeas { "network peerdns setup eth0":
     context => "/files/etc/sysconfig/network-scripts/ifcfg-eth0",
     incl => "/etc/sysconfig/network-scripts/ifcfg-eth0",
-    load_path => [ "/usr/share/augeas/lenses/dist" ],
     lens => "Shellvars.lns",
     changes => [
       "set PEERDNS no",
@@ -11,7 +10,6 @@ class stringed::general::rackspace {
   augeas { "network peerdns setup eth1":
     context => "/files/etc/sysconfig/network-scripts/ifcfg-eth1",
     incl => "/etc/sysconfig/network-scripts/ifcfg-eth1",
-    load_path => [ "/usr/share/augeas/lenses/dist" ],
     lens => "Shellvars.lns",
     changes => [
       "set PEERDNS no",
