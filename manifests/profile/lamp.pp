1d23e3229215e4fd63e0d2e806d3a27b52c5f5bf
JCotton1123 <jcotton1123@gmail.com>
2014-02-27 14:48:15 -0500

    Update lamp.pp

--- manifests/profile/lamp.pp
+++ manifests/profile/lamp.pp
@@ -3,6 +3,8 @@ class stringed::profile::lamp (
 )
 {
   include stringed::wrapper::apache_phpfpm
-  include stringed::wrapper::mysql
+  class { ::mysql::server:
+    override_options => hiera_hash('stringed::profile::lamp::mysql_override_options', $override_options)
+  }
   create_resources(firewall, $firewall_rules)
 }
