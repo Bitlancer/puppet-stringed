f874d1cbb535c5e3a170a49c52e4039adb966bad
JCotton1123 <jcotton1123@gmail.com>
2014-04-19 11:37:46 -0400

    Update apache.pp

--- manifests/profile/apache.pp
+++ manifests/profile/apache.pp
@@ -1,13 +1,13 @@
 class stringed::profile::apache (
   $listen = ['80'],
-  $name_virtal_hosts = ['*:80'],
+  $name_virtual_hosts = ['*:80'],
   $modules = [],
   $vhosts = {},
   $firewall_rules = {}
 ) {
- 
+
   include ::apache
- 
+
   apache::listen { $listen: }
   apache::namevirtualhost { $name_virtual_hosts: }
   create_resources(apache::vhosts, $vhosts)
