4c3c856b1c21824d8222759d015f48e58266e69b
JCotton1123 <jcotton1123@gmail.com>
2014-04-18 23:48:35 -0400

    Update python.pp

--- manifests/profile/python.pp
+++ manifests/profile/python.pp
@@ -1,20 +1,11 @@
 class stringed::profile::python (
-  $version = 'system',
-  $dev = true,
-  $virtualenv = true,
   $virtual_envs = {},
   $pip_packages = {},
-  $gunicorn = false,
   $gunicorn_vhosts = {},
   $firewall_rules = {}
 ) {
 
-  class { ::python:
-    version => $version,
-    dev => $dev,
-    virtualenv => $virtualenv,
-    gunicorn => $gunicorn
-  }
+  include ::python
   create_resources(python::virtualenv, hiera_hash('stringed::profile::python::virtual_envs', $virtual_envs))
   create_resources(python::pip, hiera_hash('stringed::profile::python::pip_packages', $pip_packages))
   create_resources(python::gunicorn, hiera_hash('stringed::profile::python::gunicorn_vhosts', $gunicorn_vhosts))
